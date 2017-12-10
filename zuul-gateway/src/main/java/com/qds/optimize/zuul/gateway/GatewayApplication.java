5188958ed05928cceed9daec89f46a5c12c2a92c
zhanghongtu <zhanghongtu@quandashi.com>
2017-12-10 17:37:30 +0800

    使用zuul的注解@EnableZuulProxy

--- zuul-gateway/src/main/java/com/qds/optimize/zuul/gateway/GatewayApplication.java
+++ zuul-gateway/src/main/java/com/qds/optimize/zuul/gateway/GatewayApplication.java
@@ -3,12 +3,14 @@ package com.qds.optimize.zuul.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * zuul 网关入口类
  *
  * @author neutron
  */
+@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class GatewayApplication {
